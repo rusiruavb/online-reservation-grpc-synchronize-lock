bc549ec4317fc3ef7818904458e281a99b0e9a8f
rusiruavb <rusiruavb98@gmail.com>
2024-05-07 02:40:30 +0530

    first commit with distributed locker

---
+++ src/main/java/org/synchronization/lock/ZooKeeperClient.java
@@ -0,0 +1,43 @@
+package org.synchronization.lock;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+public class ZooKeeperClient {
+    private final ZooKeeper zooKeeper;
+    public ZooKeeperClient(String zooKeeperUrl, int
+            sessionTimeout, Watcher watcher) throws IOException {
+        zooKeeper = new ZooKeeper(zooKeeperUrl,
+                sessionTimeout, watcher);
+    }
+    public String createNode(String path, boolean shouldWatch, CreateMode mode, byte[] data) throws KeeperException, InterruptedException, UnsupportedEncodingException {
+        return zooKeeper.create(path, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, mode);
+    }
+
+    public byte[] getData(String path, boolean shouldWatch) throws KeeperException, InterruptedException {
+        return zooKeeper.getData(path, shouldWatch, null);
+    }
+
+    public boolean CheckExists(String path) throws
+            KeeperException, InterruptedException {
+        Stat nodeStat = zooKeeper.exists(path, false);
+        return (nodeStat != null);
+    }
+    public void delete(String path) throws
+            KeeperException, InterruptedException {
+        zooKeeper.delete(path, -1);
+    }
+    public List<String> getChildrenNodePaths (String
+                                                      root) throws KeeperException, InterruptedException {
+        zooKeeper.getState().toString();
+        return zooKeeper.getChildren(root, false);
+    }
+    public void addWatch(String path) throws
+            KeeperException, InterruptedException {
+        zooKeeper.exists(path, true);
+    }
+}
